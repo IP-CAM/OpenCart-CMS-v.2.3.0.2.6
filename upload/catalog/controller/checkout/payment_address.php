61a2e7bbc2dc75a3853363d1b6e403487437947b
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-09-17 06:16:01 +0200

    $this->config->get('config_zone_id')

--- upload/catalog/controller/checkout/payment_address.php
+++ upload/catalog/controller/checkout/payment_address.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -46,7 +46,7 @@ class ControllerCheckoutPaymentAddress extends Controller {
 		if (isset($this->session->data['payment_address']['zone_id'])) {
 			$data['zone_id'] = $this->session->data['payment_address']['zone_id'];
 		} else {
-			$data['zone_id'] = '';
+			$data['zone_id'] = $this->config->get('config_zone_id');
 		}
 
 		$this->load->model('localisation/country');
--- upload/catalog/controller/checkout/shipping_address.php
+++ upload/catalog/controller/checkout/shipping_address.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -52,7 +52,7 @@ class ControllerCheckoutShippingAddress extends Controller {
 		if (isset($this->session->data['shipping_address']['zone_id'])) {
 			$data['zone_id'] = $this->session->data['shipping_address']['zone_id'];
 		} else {
-			$data['zone_id'] = '';
+			$data['zone_id'] = $this->config->get('config_zone_id');
 		}
 
 		$this->load->model('localisation/country');
