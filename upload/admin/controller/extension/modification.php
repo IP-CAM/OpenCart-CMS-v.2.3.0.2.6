7d6bdd3341877e34dcc30ce831b846e03a6ca986
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-12-23 00:40:54 +0200

    fix
    Не знаю зачем закомментили, но при обновлении модификаторов, если правки вносятся в админ панели, то могут быть ошибки т.к. не все файлы были ещё обновлены, в результате половина нового кода может не работать.

--- upload/admin/controller/extension/modification.php
+++ upload/admin/controller/extension/modification.php
@@ -432,9 +432,7 @@ class ControllerExtensionModification extends Controller {
 			$this->model_setting_setting->editSettingValue('config', 'config_maintenance', $maintenance);
 
 			// Do not return success message if refresh() was called with $data
-			//if (!empty($data['redirect'])) {
-				$this->session->data['success'] = $this->language->get('text_success');
-			//}
+			$this->session->data['success'] = $this->language->get('text_success');
 
 			$url = '';
 
