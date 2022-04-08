7b8bbd756248241086af32811173ae4c4fa03335
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-04-08 16:56:40 +0200

    config_action_not_found
    добавляем возможность менять нежелательные контроллеры на контроллер 404 страніцы

--- upload/catalog/controller/startup/router.php
+++ upload/catalog/controller/startup/router.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2022.
+// *	@forum		http://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -12,30 +12,38 @@ class ControllerStartupRouter extends Controller {
 		} else {
 			$route = $this->config->get('action_default');
 		}
-		
+
 		// Sanitize the call
 		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
-		
+
+		$config_seo_url_hide_controller = $this->config->get('config_action_not_found');
+		if ($config_seo_url_hide_controller) {
+			$hide_controller = explode("\r\n", $config_seo_url_hide_controller);
+			if (in_array($route, $hide_controller)) {
+				$route = $this->config->get('action_error');
+			}
+		}
+
 		// Trigger the pre events
 		$result = $this->event->trigger('controller/' . $route . '/before', array(&$route, &$data));
-		
+
 		if (!is_null($result)) {
 			return $result;
 		}
-		
+
 		// We dont want to use the loader class as it would make an controller callable.
 		$action = new Action($route);
-		
+
 		// Any output needs to be another Action object.
 		$output = $action->execute($this->registry); 
-		
+
 		// Trigger the post events
 		$result = $this->event->trigger('controller/' . $route . '/after', array(&$route, &$data, &$output));
-		
+
 		if (!is_null($result)) {
 			return $result;
 		}
-		
+
 		return $output;
 	}
 }
