0e03aaa26bf1613f9082f33adf2f13906749a84d
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2023-07-31 17:12:24 +0300

    Update encryption.php

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -1,5 +1,5 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@copyright	OPENCART.PRO 2011 - 2023.
 // *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
@@ -14,7 +14,7 @@ final class Encryption {
 	public function encrypt($value) {
 		if (version_compare(phpversion(), '7.1.0', '>') == true || !function_exists('mcrypt_encrypt')) {
 			// для улучшения сюда: https://www.php.net/manual/ru/function.openssl-encrypt.php
-			return strtr(base64_encode(openssl_encrypt($value, 'aes-128-cbc', hash('sha256', hex2bin($this->key), true), 0, hex2bin(substr(hash_hmac('sha256', $this->key, hash('sha256', $this->key, true)), 0, 32)))), '+/=', '-_,');
+			return strtr(base64_encode(openssl_encrypt($value, 'aes-128-cbc', hash('sha256', $this->key, true), 0, hex2bin(substr(hash_hmac('sha256', $this->key, hash('sha256', $this->key, true)), 0, 32)))), '+/=', '-_,');
 		} else {
 			return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, hash('sha256', hash('sha256', $this->key, true), true), $value, MCRYPT_MODE_ECB)), '+/=', '-_,');
 		}
@@ -23,7 +23,7 @@ final class Encryption {
 	public function decrypt($value) {
 		if (version_compare(phpversion(), '7.1.0', '>') == true || !function_exists('mcrypt_encrypt')) {
 			// для улучшения сюда: https://www.php.net/manual/ru/function.openssl-encrypt.php
-			return trim(openssl_decrypt(base64_decode(strtr($value, '-_,', '+/=')), 'aes-128-cbc', hash('sha256', hex2bin($this->key), true), 0, hex2bin(substr(hash_hmac('sha256', $this->key, hash('sha256', $this->key, true)), 0, 32))));
+			return trim(openssl_decrypt(base64_decode(strtr($value, '-_,', '+/=')), 'aes-128-cbc', hash('sha256', $this->key, true), 0, hex2bin(substr(hash_hmac('sha256', $this->key, hash('sha256', $this->key, true)), 0, 32))));
 		} else {
 			return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash('sha256', hash('sha256', $this->key, true), true), base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB));
 		}
