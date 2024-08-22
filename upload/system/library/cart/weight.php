454c81223ea6b622b5764172c39d283655453ba7
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2024-08-22 16:01:29 +0300

    Add files via upload

--- upload/system/library/cart/weight.php
+++ upload/system/library/cart/weight.php
@@ -1,10 +1,12 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2024.
+// *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 namespace Cart;
+//https://stackoverflow.com/questions/74878889/php-warning-deprecated-creation-of-dynamic-property-is-deprecated
+#[\AllowDynamicProperties]
 class Weight {
 	private $weights = array();
 
