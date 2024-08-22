8a0adecff6131735771198d9ed28362fd98a3891
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2024-08-22 15:59:27 +0300

    Add files via upload

--- upload/system/library/session/native.php
+++ upload/system/library/session/native.php
@@ -1,10 +1,12 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2022.
+// *	@copyright	OPENCART.PRO 2011 - 2024.
 // *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 namespace Session;
+//https://stackoverflow.com/questions/74878889/php-warning-deprecated-creation-of-dynamic-property-is-deprecated
+#[\AllowDynamicProperties]
 class Native extends \SessionHandler {
 	public function __construct($registry) {
 		$this->config = $registry->get('config');
