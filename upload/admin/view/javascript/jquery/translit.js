11b738b5752ea26fcc603a10eb694a6967d323cc
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2024-08-27 02:44:41 +0300

    добавлены буквы бел. мовы
    добавлены буквы бел. мовы и также изменены буквы для общих правил яндекса и гугл

--- upload/admin/view/javascript/jquery/translit.js
+++ upload/admin/view/javascript/jquery/translit.js
@@ -1,24 +1,32 @@
 var ru2en = {
-  ru_str : 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя(),.; "+/*',
-  en_str : ['a','b','v','g','d','e','jo','zh','z','i','j','k','l','m','n','o','p','r','s','t','u','f',
-    'h','c','ch','sh','shh','','i','','je','ju','ja',
-    'a','b','v','g','d','e','jo','zh','z','i','j','k','l','m','n','o','p','r','s','t','u','f',
-    'h','c','ch','sh','shh','','i','','je','ju','ja',
-    '-', '-','-', '-', '-','-','-','-','-','-'],
-  translit : function(org_str) {
-    
-    var tmp_str = "";
-    for(var i = 0, l = org_str.length; i < l; i++) {
-      var s = org_str.charAt(i), n = this.ru_str.indexOf(s);
-      if(n >= 0) { tmp_str += this.en_str[n]; }
-      else { tmp_str += s; }
-    }
-    return tmp_str.toLowerCase();
-  }
-}
+	ru_str : 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯЎІабвгдеёжзийклмнопрстуфхцчшщъыьэюяўі(),.; "+/*',
+	en_str : [
+	'a','b','v','g','d','e','jo','zh','z','i','j','k','l','m','n','o','p','r','s','t','u','f',
+	'h','c','ch','sh','shch','','y','','e','yu','ya','u','i',
+	'a','b','v','g','d','e','jo','zh','z','i','j','k','l','m','n','o','p','r','s','t','u','f',
+	'h','c','ch','sh','shch','','y','','e','yu','ya','u','i',
+	'','','-','-','-','-','','plus','-','-'
+	],
+	translit : function(org_str) {
+		var tmp_str = '', i;
+
+		for (i = 0, l = org_str.length; i < l; i++) {
+			var s = org_str.charAt(i), n = ru2en.ru_str.indexOf(s);
+
+			if(n >= 0) {
+				tmp_str += ru2en.en_str[n];
+			} else {
+				tmp_str += s;
+			}
+		}
+
+		return tmp_str.toLowerCase();
+	}
+};
 
 function setTranslit(source, dest, rewrite) {
 	var name = $("input[name='"+source+"']").val();
+
 	if (name != undefined) {
 		$("input[name='"+source+"']").change(function(){
 			var name = $("input[name='"+source+"']").val();
@@ -32,8 +40,9 @@ function setTranslit(source, dest, rewrite) {
 $(document).ready(function(){
 	// Products
 	setTranslit("product_description\\[1\\]\\[name\\]", "keyword", false);
-	setTranslit("article_description\\[1\\]\\[name\\]", "keyword", false);
 	// Info Articles
+	setTranslit("article_description\\[1\\]\\[name\\]", "keyword", false);
+	// Info Information
 	setTranslit("information_description\\[1\\]\\[title\\]", "keyword", false);
 	// Categories
 	setTranslit("category_description\\[1\\]\\[name\\]", "keyword", false);
