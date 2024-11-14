1e2e6a43453fc34aa9341f4dc302d25ac50cd1e0
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2024-11-14 03:55:15 +0300

    Update blog_popular.php

--- upload/catalog/language/en-gb/extension/module/blog_popular.php
+++ upload/catalog/language/en-gb/extension/module/blog_popular.php
@@ -1,8 +1,15 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2024.
+// *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Heading 
 $_['heading_title'] = 'Popular Articles';
+
+// Text
+$_['text_views'] 	  = 'Reviews:';
+$_['text_reviews']  = 'Based on %s reviews.';
+
+// Button
+$_['button_more']   = 'more';
