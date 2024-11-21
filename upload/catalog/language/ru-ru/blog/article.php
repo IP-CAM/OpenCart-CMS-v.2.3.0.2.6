ff8297958c21e0a3cb17c634c6aeff6a37734a6a
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2024-11-21 05:41:57 +0300

    Update article.php

--- upload/catalog/language/ru-ru/blog/article.php
+++ upload/catalog/language/ru-ru/blog/article.php
@@ -1,30 +1,32 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@copyright	OPENCART.PRO 2011 - 2024.
 // *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Text
-$_['text_blog']            = 'Блог';
+$_['text_blog']            = 'Форум';
 $_['text_write']           = 'Написать отзыв';
 $_['text_login']           = 'Пожалуйста <a href="%s">авторизируйтесь</a> или <a href="%s">создайте учетную запись</a> перед тем как написать отзыв';
 $_['text_no_reviews']      = 'Нет отзывов на эту статью.';
 $_['text_on']              = ' от ';
-$_['text_note']            = '<span style="color: #FF0000;">Внимание:</span> HTML не поддерживается! Используйте обычный текст.';
+$_['text_note']            = '<span class="text-danger">Примечание:</span> HTML разметка не поддерживается! Используйте обычный текст.';
 $_['text_success']         = 'Спасибо за ваш отзыв. После проверки администратором он будет опубликован.';
 $_['text_wait']            = 'Пожалуйста, подождите!';
-$_['text_reviews']         = 'Отзывов: %s';
+$_['text_reviews']         = 'Комментариев: %s';
 $_['text_error']           = 'Статья не найдена!';
 $_['text_views']           = 'Просмотров:';
 $_['button_more']          = 'подробнее';
 $_['text_tax']             = 'Без НДС:';
 $_['text_related']         = 'Похожие статьи';
-$_['text_related_product'] = 'Сопутствующие Товары';
+$_['text_related_product'] = 'Сопутствующие объявления';
 $_['text_benefits']        = 'Преимущества:';
+$_['text_author']          = 'Автор:';
+$_['text_date']            = 'Дата публикации:';
 
 // Entry
-$_['entry_name']           = 'Ваше Имя:';
-$_['entry_review']         = 'Ваш отзыв:';
+$_['entry_name']           = 'Ваше Имя';
+$_['entry_review']         = 'Ваш отзыв';
 $_['entry_rating']         = 'Оценка:';
 $_['entry_good']           = 'Хорошо';
 $_['entry_bad']            = 'Плохо';
