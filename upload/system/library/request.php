b7e53afa7b781eb09686276e168e4b705c46f954
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2024-12-30 22:49:51 +0300

    Update request.php

--- upload/system/library/request.php
+++ upload/system/library/request.php
@@ -1,12 +1,13 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2024.
+// *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 class Request {
 	public $get = array();
 	public $post = array();
+	public $request = array();
 	public $cookie = array();
 	public $files = array();
 	public $server = array();
